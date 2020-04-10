d415837fd375c99bf2acc7a1c1e46180a77bc670
Rasmus Bonnedal <rasmus.bonnedal@gmail.com>
2020-04-10 23:08:17 +0200

    Add debug to generate projects

--- generate_projects.sh
+++ generate_projects.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mkdir build
 pushd build
-# set CMAKE_PREFIX_PATH="C:/Qt/5.10.1/msvc2017_64/"
-# set CMAKE_PREFIX_PATH=
-cmake ..
+cmake .. -G Ninja
 popd
+mkdir build_debug
+pushd build_debug
+cmake -DCMAKE_BUILD_TYPE=Debug .. -G Ninja
+popd
+ 
