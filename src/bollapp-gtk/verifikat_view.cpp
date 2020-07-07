4d3518088c641b6171d00fbe5c49b201919fdd65
Rasmus Bonnedal <rasmus.bonnedal@gmail.com>
2020-07-07 22:24:40 +0200

    Refactor VerifikatView

---
+++ src/bollapp-gtk/verifikat_view.cpp
@@ -0,0 +1,229 @@
+#include "verifikat_view.h"
+
+#include "cellrenderertextcompletion.h"
+
+VerifikatView::VerifikatView()
+    : Gtk::TreeView(), m_completion(createEntryCompletion()) {
+    m_refTreeModel = Gtk::ListStore::create(m_columns);
+    set_model(m_refTreeModel);
+    m_columns.addColumns(*this);
+    // enableEditNextField({get_column(0), get_column(1)});
+    clear();
+}
+
+void VerifikatView::clear() { m_refTreeModel->clear(); }
+
+void VerifikatView::addRow(unsigned konto, const Pengar& pengar,
+                           const Date& date, const std::optional<Date>& struken,
+                           bool first) {
+    Gtk::TreeIter treeIter =
+        first ? m_refTreeModel->prepend() : m_refTreeModel->append();
+    m_columns.setRow(*treeIter, konto, pengar, date, struken);
+}
+
+void VerifikatView::updateKontoLista(
+    const std::map<int, BollDoc::Konto>& kontoplan) {
+    auto listStore =
+        Glib::RefPtr<Gtk::ListStore>::cast_dynamic(m_completion->get_model());
+    listStore->clear();
+    m_kontoplan.clear();
+
+    for (const auto& it : kontoplan) {
+        auto row = *(listStore->append());
+        m_completionRecord.setRow(row, std::to_string(it.first),
+                                  it.second.getText());
+        m_kontoplan[it.first] = it.second.getText();
+    }
+}
+
+sigc::signal<void, const std::vector<BollDoc::Rad>&>
+VerifikatView::signalEdited() {
+    return m_signalEdited;
+}
+
+void VerifikatView::sendEditedSignal() {
+    std::cout << "sendEditedSignal()" << std::endl;
+    std::vector<BollDoc::Rad> result;
+    for (const auto& row : m_refTreeModel->children()) {
+        unsigned konto;
+        Pengar pengar;
+        Date date;
+        std::optional<Date> struken;
+        m_columns.getRow(row, konto, pengar, date, struken);
+        if (pengar.get() != 0 && konto != 0) {
+            result.emplace_back(date, konto, pengar, struken);
+        }
+    }
+    m_signalEdited.emit(result);
+}
+
+Glib::RefPtr<Gtk::EntryCompletion> VerifikatView::createEntryCompletion() {
+    Glib::RefPtr<Gtk::EntryCompletion> completion =
+        Gtk::EntryCompletion::create();
+    Glib::RefPtr<Gtk::ListStore> listStore =
+        Gtk::ListStore::create(m_completionRecord);
+    completion->set_model(listStore);
+    completion->set_text_column(m_completionRecord.m_colText);
+    completion->pack_start(m_completionCellRenderer);
+    completion->add_attribute(m_completionCellRenderer.property_text(),
+                              m_completionRecord.m_colText2);
+    //        completion->set_popup_completion(true);
+    completion->set_inline_selection(true);
+    return completion;
+}
+
+// Convert the int konto to a string with kontonummer and text
+// Set formatting of the cell
+void VerifikatView::onCellDataKontoColumn(
+    Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter) {
+    Gtk::TreeModel::Row row = *iter;
+    Gtk::CellRendererText* crt = dynamic_cast<Gtk::CellRendererText*>(cell);
+
+    bool struken = row[m_columns.m_colStruken];
+    crt->property_strikethrough() = struken;
+    crt->property_editable() = !struken;
+
+    unsigned int konto = row[m_columns.m_colKonto];
+    if (konto == 0) {
+        crt->property_foreground_rgba() = Gdk::RGBA("#bbb");
+        crt->property_text() = "Ny kontering";
+        return;
+    } else {
+        crt->property_foreground_rgba() = Gdk::RGBA("#000");
+    }
+
+    auto it = m_kontoplan.find(konto);
+    std::string kontoText = "(Ogiltigt konto)";
+    if (it != m_kontoplan.end()) {
+        kontoText = it->second;
+    }
+    crt->property_text() = std::to_string(konto) + "     " + kontoText;
+}
+
+void VerifikatView::onCellDataPengarColumn(
+    Gtk::CellRenderer* cell, const Gtk::TreeModel::iterator& iter) {
+    Gtk::TreeModel::Row row = *iter;
+    Gtk::CellRendererText* crt = dynamic_cast<Gtk::CellRendererText*>(cell);
+    bool struken = row[m_columns.m_colStruken];
+    crt->property_strikethrough() = struken;
+    crt->property_editable() = !struken;
+
+    unsigned int konto = row[m_columns.m_colKonto];
+    if (konto == 0) {
+        crt->property_foreground_rgba() = Gdk::RGBA("#bbb");
+    } else {
+        crt->property_foreground_rgba() = Gdk::RGBA("#000");
+    }
+
+    Pengar pengar(row[m_columns.m_colPengar]);
+    crt->property_text() = toString(pengar) + " kr";
+}
+
+void VerifikatView::onEditedKonto(const Glib::ustring& path_string,
+                                  const Glib::ustring& new_text) {
+    std::cout << "VerifikatView::onEdited(" << path_string << ")" << std::endl;
+    int new_value = atoi(new_text.c_str());
+    if (new_value && m_kontoplan.find(new_value) != m_kontoplan.end()) {
+        Gtk::TreePath path(path_string);
+        Gtk::TreeModel::iterator iter = m_refTreeModel->get_iter(path);
+        if (iter) {
+            Gtk::TreeModel::Row row = *iter;
+            row[m_columns.m_colKonto] = new_value;
+            sendEditedSignal();
+        }
+    }
+}
+
+void VerifikatView::onEditedPengar(const Glib::ustring& path_string,
+                                   const Glib::ustring& new_text) {
+    std::cout << "VerifikatView::onEditedPengar(" << path_string << ")"
+              << std::endl;
+    Gtk::TreePath path(path_string);
+    Gtk::TreeModel::iterator iter = m_refTreeModel->get_iter(path);
+    if (iter) {
+        Gtk::TreeModel::Row row = *iter;
+        unsigned konto = row[m_columns.m_colKonto];
+        try {
+            Pengar p = parsePengar(new_text);
+            row[m_columns.m_colPengar] = p;
+            if (path_string == "0" && konto != 0 && p.get() != 0) {
+                addRow(0, 0, Date(), std::nullopt, true);
+            }
+            sendEditedSignal();
+        } catch (std::exception&) {
+        }
+    }
+}
+
+//
+// Class CompletionRecord
+//
+VerifikatView::CompletionRecord::CompletionRecord() {
+    add(m_colText);
+    add(m_colText2);
+}
+
+void VerifikatView::CompletionRecord::setRow(Gtk::TreeRow& row,
+                                             const std::string& s,
+                                             const std::string& s2) {
+    row[m_colText] = s;
+    row[m_colText2] = s2;
+}
+
+//
+// Class ModelColumns
+//
+VerifikatView::ModelColumns::ModelColumns() {
+    add(m_colKonto);
+    add(m_colPengar);
+    add(m_colDate);
+    add(m_colStruken);
+    add(m_colStrukenDate);
+}
+
+void VerifikatView::ModelColumns::addColumns(VerifikatView& treeView) {
+    auto& cell = treeView.m_kontoCellRenderer;
+    cell.property_editable() = true;
+    cell.signal_edited().connect(
+        sigc::mem_fun(&treeView, &VerifikatView::onEditedKonto));
+    makeCellRendererUseCompletion(cell, treeView.m_completion);
+    treeView.append_column("Konto", cell);
+    Gtk::TreeViewColumn* column = treeView.get_column(0);
+    column->set_cell_data_func(
+        cell, sigc::mem_fun(&treeView, &VerifikatView::onCellDataKontoColumn));
+
+    auto& pengarCell = treeView.m_pengarCellRenderer;
+    treeView.append_column("Debet / Kredit", pengarCell);
+    pengarCell.signal_edited().connect(
+        sigc::mem_fun(&treeView, &VerifikatView::onEditedPengar));
+    column = treeView.get_column(1);
+    column->set_cell_data_func(
+        pengarCell,
+        sigc::mem_fun(&treeView, &VerifikatView::onCellDataPengarColumn));
+}
+
+void VerifikatView::ModelColumns::setRow(
+    const Gtk::TreeRow& row, unsigned konto, Pengar pengar, const Date& date,
+    const std::optional<Date>& struken) const {
+    row[m_colKonto] = konto;
+    row[m_colPengar] = pengar;
+    row[m_colDate] = date;
+    row[m_colStruken] = struken.has_value();
+    if (struken.has_value()) {
+        row[m_colStrukenDate] = *struken;
+    }
+}
+
+void VerifikatView::ModelColumns::getRow(const Gtk::TreeRow& row,
+                                         unsigned& konto, Pengar& pengar,
+                                         Date& date,
+                                         std::optional<Date>& struken) const {
+    konto = row[m_colKonto];
+    pengar = row[m_colPengar];
+    date = row[m_colDate];
+    if (row[m_colStruken]) {
+        struken = row[m_colStrukenDate];
+    } else {
+        struken.reset();
+    }
+}
---
+++ src/bollapp-gtk/verifikat_view.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "bolldoc.h"
+#include "date.h"
+#include "pengar.h"
+
+#include <gtkmm.h>
+
+#include <map>
+#include <string>
+#include <vector>
+
+class VerifikatView : public Gtk::TreeView {
+public:
+    VerifikatView();
+    void clear();
+    void addRow(unsigned konto, const Pengar& pengar, const Date& date,
+                const std::optional<Date>& struken, bool first = false);
+    void updateKontoLista(const std::map<int, BollDoc::Konto>& kontoplan);
+    sigc::signal<void, const std::vector<BollDoc::Rad>&> signalEdited();
+
+private:
+    void sendEditedSignal();
+    Glib::RefPtr<Gtk::EntryCompletion> createEntryCompletion();
+
+    // Convert the int konto to a string with kontonummer and text
+    // Set formatting of the cell
+    void onCellDataKontoColumn(Gtk::CellRenderer* cell,
+                               const Gtk::TreeModel::iterator& iter);
+    void onCellDataPengarColumn(Gtk::CellRenderer* cell,
+                                const Gtk::TreeModel::iterator& iter);
+    void onEditedKonto(const Glib::ustring& path_string,
+                       const Glib::ustring& new_text);
+    void onEditedPengar(const Glib::ustring& path_string,
+                        const Glib::ustring& new_text);
+
+    class CompletionRecord : public Gtk::TreeModel::ColumnRecord {
+    public:
+        CompletionRecord();
+        void setRow(Gtk::TreeRow& row, const std::string& s,
+                    const std::string& s2);
+        Gtk::TreeModelColumn<Glib::ustring> m_colText;
+        Gtk::TreeModelColumn<Glib::ustring> m_colText2;
+    };
+    CompletionRecord m_completionRecord;
+    Gtk::CellRendererText m_completionCellRenderer;
+
+    // Model for a row of a Verifikat
+    class ModelColumns : public Gtk::TreeModel::ColumnRecord {
+    public:
+        ModelColumns();
+        void addColumns(VerifikatView& treeView);
+        void setRow(const Gtk::TreeRow& row, unsigned konto, Pengar pengar,
+                    const Date& date, const std::optional<Date>& struken) const;
+        void getRow(const Gtk::TreeRow& row, unsigned& konto, Pengar& pengar,
+                    Date& date, std::optional<Date>& struken) const;
+        Gtk::TreeModelColumn<unsigned> m_colKonto;
+        Gtk::TreeModelColumn<Pengar> m_colPengar;
+        Gtk::TreeModelColumn<Date> m_colDate;
+        Gtk::TreeModelColumn<bool> m_colStruken;
+        Gtk::TreeModelColumn<Date> m_colStrukenDate;
+    };
+
+    ModelColumns m_columns;
+    Glib::RefPtr<Gtk::ListStore> m_refTreeModel;
+    Glib::RefPtr<Gtk::EntryCompletion> m_completion;
+    Gtk::CellRendererText m_kontoCellRenderer;
+    Gtk::CellRendererText m_pengarCellRenderer;
+    std::map<unsigned int, std::string> m_kontoplan;
+    sigc::signal<void, const std::vector<BollDoc::Rad>&> m_signalEdited;
+};
