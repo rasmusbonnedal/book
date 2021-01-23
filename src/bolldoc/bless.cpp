2482b0f2351f310d30bd4bf67cb413f426d56ecc
Rasmus Bonnedal <rasmus.bonnedal@gmail.com>
2021-01-23 21:28:13 +0100

    Add error handling to bless

--- src/bolldoc/bless.cpp
+++ src/bolldoc/bless.cpp
@@ -1,7 +1,10 @@
 #include "serialize.h"
 
+#include <rapidxml.hpp>
+
 #include <fstream>
 #include <iostream>
+#include <memory>
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -9,12 +12,22 @@ int main(int argc, char** argv) {
                   << "Fix checksum for bolldoc file" << std::endl;
         return 1;
     }
+
     std::ifstream ifs(argv[1]);
     if (!ifs.good()) {
         std::cout << "Could not open " << argv[1] << " for reading" << std::endl;
         return 1;
     }
-    BollDoc doc = Serialize::loadDocument(ifs, true);
+    std::unique_ptr<BollDoc> doc;
+    try {
+        doc = std::make_unique<BollDoc>(Serialize::loadDocument(ifs, true));
+    } catch (rapidxml::parse_error& e) {
+        std::cout << "XML Parse error: " << e.what() << std::endl;
+
+        std::string where(e.where<char>(), 1);
+        std::cout << "Found at: " << where << std::endl;
+        return 1;
+    }
     ifs.close();
 
     std::ofstream ofs(argv[1]);
@@ -22,7 +35,7 @@ int main(int argc, char** argv) {
         std::cout << "Could not open " << argv[1] << " for writing" << std::endl;
         return 1;
     }
-    Serialize::saveDocumentCustom(doc, ofs);
+    Serialize::saveDocumentCustom(*doc, ofs);
     std::cout << "Checksum fixed!" << std::endl;
     return 0;
 }
